3968f27c6a012bb14d2f5fbd69e9036ec6fab37e
ThrisRH <144113163+ThrisRH@users.noreply.github.com>
2024-10-09 19:21:12 +0700

    Check loginpassword với database

--- src/screens/Auth/login.tsx
+++ src/screens/Auth/login.tsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Alert, Image, StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, ToastAndroid, View } from "react-native";
 import HeroSection from "../../components/sections/HeroSection";
 import Input, {
   InputWithIcon,
   PhoneInput,
 } from "../../components/Inputs/Inputs";
-import ButtonFill, { ButtonLine } from "../../components/Buttons/Buttons";
+import ButtonFill from "../../components/Buttons/Buttons";
 import TextAndFunction from "../../components/Items/TextAndFunction";
 import IconAndText from "../../components/Items/IconAndText";
 
 // Import Icons
 import PasswordIC from "../../svg/MTri/PasswordIC";
-import FingerPrintIC from "../../svg/MTri/FingerPrintIC";
 import OTPIC from "../../svg/MTri/OtpIC";
 import {
   NavigationProp,
@@ -34,6 +33,7 @@ const Login = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
   const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const [warning, setWarning] = useState<string>("");
   const [withPassword, setWithPassword] = useState(false);
   const handleLogin = () => {
@@ -54,11 +54,34 @@ const Login = () => {
       setWarning("Số điện thoại phải đủ 10 số");
     }
   };
-  const handleCheckPassword = () => {
-    navigation.reset({
-      index: 0,
-      routes: [{ name: "HomePage" }],
-    });
+  const handleCheckPassword = async () => {
+    const phoneNumberTostring = String(phoneNumber);
+    try {
+      const response = await fetch("http://192.168.1.10:5000/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ phonenumber: phoneNumberTostring, password }),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        data.erCode === "wrp" && setWarning("Sai mật khẩu!"); // wrp: wrong password
+        data.erCode === "pne" && setWarning("Số điện thoại không tồn tại!"); // pne: phone not existed
+      }
+      if (response.ok) {
+        ToastAndroid.show(data.message, ToastAndroid.SHORT);
+        // Chuyển trang khi thành công
+
+        navigation.reset({
+          index: 0,
+          routes: [{ name: "HomePage" }],
+        });
+      }
+    } catch (error) {
+      console.error("Error collecting data:", error);
+    }
   };
   return (
     <View className="w-full h-full flex flex-col items-center justify-center px-6 gap-6">
@@ -75,13 +98,6 @@ const Login = () => {
             placeholder="Số điện thoại"
           />
           {/* Hiển thị cảnh báo */}
-          {warning ? (
-            <View>
-              <Text className="text-red-500">* {warning} !</Text>
-            </View>
-          ) : (
-            ""
-          )}
         </View>
 
         {/* Phương thức đăng nhập với mật khẩu */}
@@ -90,7 +106,16 @@ const Login = () => {
             isPassword={true}
             inputType="default"
             placeholder="Mật khẩu"
+            value={password}
+            onChangeText={setPassword}
           />
+        ) : (
+          ""
+        )}
+        {warning ? (
+          <View>
+            <Text className="text-red-500">* {warning}</Text>
+          </View>
         ) : (
           ""
         )}
